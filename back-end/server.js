feb763a75f2d2f89e47abb41640d83f3fc9b8634
Devam <dns682@usask.ca>
2024-01-23 23:26:47 -0600

    update backend docker setup

--- back-end/server.js
+++ back-end/server.js
@@ -10,14 +10,33 @@ const app = express();
 
 // create connection to mysql
 
-var connection = mysql.createConnection({
-  host     : 'mysql',
-  user     : 'root',
-  password : 'CMPT371-Team1'
-});
+// var connection = mysql.createConnection({
+//   host     : 'mysql',
+//   user     : 'root',
+//   password : 'CMPT371-Team1'
+// });
  
-connection.connect();
+// connection.connect();
    
-app.listen(PORT, HOST);
+// app.listen(PORT, HOST);
 
-console.log('up and running');
+// console.log('up and running');
+
+
+// Create a connection pool to mysql
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'mysql',
+  user: 'root',
+  password: 'CMPT371-Team1'
+});
+
+// Use middleware to add the pool to every request
+app.use((req, res, next) => {
+  req.mysqlPool = pool;
+  next();
+});
+
+app.listen(PORT, HOST, () => {
+  console.log('up and running');
+});
