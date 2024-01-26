492bd738197d6cf9fe2c8789956cfef9f02bbd02
Aryan Mehta <aryanmht9@gmail.com>
2024-01-26 00:56:00 -0600

    Logging have been set up for the backend

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -5,19 +5,31 @@ import { TestService } from "./adapter/TestService";
 import { UserSQL } from "./adapter/UserSQL";
 import { UserOther } from "./adapter/UserOther";
 
+//logging 
+import log4js from 'log4js';
+import {getLogger, configure} from 'log4js';
+
+configure('src/log4js-config.json')
+
 const config =  require('./config.ts');
 const express =  require('express');
 const app =  express();
+const info_logger = getLogger('info'); // logger for info
+const err_logger = getLogger('error'); // logger for error
+const debug_logger = getLogger('debug'); // logger for debug
 
 console.log(`NODE_ENV=${config.NODE_ENV}`);
 
 app.get('/', (req: Request, res: Response) => {
+    info_logger.info('GET request received');
     const t = container.resolve(TestService);
     t.call();
     res.send('Hello World !!');
 });
 
 app.listen(config.PORT, config.HOST, () => {
+    err_logger.error("Testing ERROR logs")
+    debug_logger.debug("Testing DEBUG logs")
     console.log(`APP LISTENING ON http://${config.HOST}:${config.PORT}`);
 })
 
