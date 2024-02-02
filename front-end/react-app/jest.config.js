e47a8a3b1f79a6ec37444ad2300cd567c5e96b32
NaronB66 <51119270+Nk-Tn-3@users.noreply.github.com>
2024-02-02 15:15:53 -0600

    issue #37: Added jest, removed node modules

--- front-end/react-app/jest.config.js
+++ front-end/react-app/jest.config.js
@@ -1,15 +1,30 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+//export {};
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+   // to obtain access to the matchers.
+   transform: {
+    '^.+\\.tsx?$': 'babel-jest',
+  },
+  
   moduleNameMapper: {
-    '\\.css$': 'identity-obj-proxy', // Mock CSS modules
-  },
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  snapshotSerializers: ['jest-emotion'],
+
+    // if your using tsconfig.paths thers is no harm in telling jest
+  '@components/(.*)$': '<rootDir>/src/components/$1',
+  '@/(.*)$': '<rootDir>/src/$1',
+    
+    // mocking assests and styling
+  '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '<rootDir>/tests/mocks/fileMock.ts',
+  '^.+\\.(css|less|scss|sass)$': '<rootDir>/tests/mocks/styleMock.ts',
+  /* mock models and services folder */
+  '(assets|models|services)': '<rootDir>/tests/mocks/fileMock.ts',
+},
+ // to obtain access to the matchers.
+setupFilesAfterEnv: ['./tests/setupTests.ts'],
+
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePaths: ['<rootDir>'],
+  testEnvironment: 'jsdom',
 };
