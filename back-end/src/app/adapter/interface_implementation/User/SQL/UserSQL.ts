903eebb90d2d31e358c7c0046b015f697ed4960f
Khoa Nguyen <minhkhoanguyen.work@gmail.com>
2024-02-11 19:43:49 -0600

    updating changes

--- back-end/src/app/adapter/interface_implementation/User/SQL/UserSQL.ts
+++
@@ -1,9 +0,0 @@
-import { IUser } from "../../../../domain/interfaces/repositories/IUserRepository";
-
-export class UserSQL implements IUser {
-
-  getAll(): void {
-    console.log("SQL Implementation of IUser");
-  }
-
-}
--- back-end/src/resources/config.ts
+++ back-end/src/resources/config.ts
@@ -5,25 +5,17 @@ dotenv.config({
   path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
 });
 
-// module.exports = {
-//   NODE_ENV: process.env.NODE_ENV ?? "development",
-//   HOST: process.env.HOST ?? "localhost",
-//   PORT: process.env.PORT ?? 3000
-// };
+console.log(path.resolve(__dirname, `${process.env.NODE_ENV}.env`));
 
 const NODE_ENV: string = process.env.NODE_ENV ?? "development";
 const HOST: string = process.env.HOST ?? "localhost";
 const PORT: number = process.env.PORT != null ? parseInt(process.env.PORT) : 3000;
-// export function nodeEnv(): string {
-//   return process.env.NODE_ENV ?? "development";
-// }
 
-// export function host(): string {
-//   return process.env.HOST ?? "localhost";
-// }
+const DB_PORT: number = process.env.DB_PORT != null ? parseInt(process.env.DB_PORT) : 3306;
+const DB_HOST: string = process.env.DB_HOST ?? "localhost";
+const DB_USER: string = process.env.DB_USER ?? "root";
+const DB_PASSWORD: string = process.env.DB_PASSWORD ?? "";
+const DB_CONNECTION_LIMIT: number = process.env.DB_CONNECTION_LIMIT != null ? parseInt(process.env.DB_CONNECTION_LIMIT) : 10;
 
-// export function port(): number {
-//   return process.env.PORT ?? 3000;
-// }
 
-export { NODE_ENV, HOST, PORT };
+export { NODE_ENV, HOST, PORT, DB_PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_CONNECTION_LIMIT };
