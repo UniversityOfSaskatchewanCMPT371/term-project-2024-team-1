46ef289d7d2bc6cebd76a93a7c9704c4a156368b
Aaryan Patel <aap738@usask.ca>
2024-02-16 14:56:28 -0600

    added sanitation and validation (and rebranched to avoid merge conflicts)

---
+++ front-end/ReactFE/src/utils/sanitationUtils.js
@@ -0,0 +1,18 @@
+/* trims whitespace and removes special characters except for basic punctuation */
+export const sanitizeNameAndProvince = (input) => {
+    return input.trim().replace(/[^a-zA-Z0-9\s,.'-]/g, '').replace(/\s{2,}/g, ' ');
+  };
+    
+  /* standardizes postal codes by uppercase and removing unnecessary spaces/dashes */
+  export const sanitizePostalCode = (postalCode) => {
+    return postalCode.toUpperCase().replace(/\s+/g, '').replace(/-/g, '');
+  };
+    
+  export const sanitizeEmail = (email) => {
+    return email.trim();
+  };
+    
+  export const sanitizePhoneNumber = (phone) => {
+    return phone.replace(/[^\d+]/g, '');
+  };
+  
