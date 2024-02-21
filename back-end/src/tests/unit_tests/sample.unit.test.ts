d838d73e14f0632716b7817deb742d7b7861fa44
Thai <thaila71669@gmail.com>
2024-02-21 16:24:23 -0600

    TEST

--- back-end/src/tests/unit_tests/sample.unit.test.ts
+++ back-end/src/tests/unit_tests/sample.unit.test.ts
@@ -1,7 +1,7 @@
 describe("Unit Sample: Class/component name test", () => {
   describe("Class/component method test", () => {
     it("Class component method test case: should do something based on something", () => {
-      expect(true).toBeTruthy();
+      expect(false).toBeTruthy();
     });
   });
 });
