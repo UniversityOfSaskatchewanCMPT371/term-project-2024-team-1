64345e8cf055d23e5d9a8ad82dc2829f06e6617f
Thai <thaila71669@gmail.com>
2024-03-01 21:44:46 -0600

    fix: fixed incorrect UserSQL methods and added test user

--- back-end/src/app/domain/interfaces/repositories/IUserRepository.ts
+++ back-end/src/app/domain/interfaces/repositories/IUserRepository.ts
@@ -3,8 +3,8 @@ import { User } from "@app/domain/User";
 
 export interface IUserRepository {
   getAll: () => Promise<User[]>;
-  getById: (id: number) => Promise<User>;
+  getById: (userId: string) => Promise<User>;
   create: (user: User) => Promise<boolean>;
   update: (user: User) => Promise<boolean>;
-  delete: (id: number) => Promise<boolean>;
+  delete: (userId: string) => Promise<boolean>;
 }
