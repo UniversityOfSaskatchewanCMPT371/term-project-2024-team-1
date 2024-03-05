20d4f2af187f6abc63d8ccee1cb90331be931308
Thai <thaila71669@gmail.com>
2024-03-05 17:05:40 -0600

    fix: fixed User domain object and UserGetAllHandler

--- back-end/src/app/adapter/Controllers/UserController.ts
+++ back-end/src/app/adapter/Controllers/UserController.ts
@@ -1,5 +1,5 @@
 
-import { ADMIN, USER, authenticate } from "@app/application/util";
+import { ADMIN, authenticate } from "@app/application/util";
 import express, { Request, Response, Router } from "express";
 import { container } from "tsyringe";
 import { UserDeleteHandler } from "./Handlers/UserDeleteHandler";
@@ -12,7 +12,7 @@ const userGetAllHandler: UserGetAllHandler = container.resolve(UserGetAllHandler
 const userDeleteHandler: UserDeleteHandler = container.resolve(UserDeleteHandler);
 const loginAuthHandler: LoginAuthHandler = container.resolve(LoginAuthHandler);
 
-router.get("/users", authenticate(USER), (req: Request, res: Response) => {
+router.get("/user", authenticate(ADMIN), (req: Request, res: Response) => {
   userGetAllHandler.handle(req, res);
 });
 
