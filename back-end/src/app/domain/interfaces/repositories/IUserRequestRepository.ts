13a2589c9f4c5f706da6c9492bb06bbd9e68da9d
Thai La <thaila71669@gmail.com>
2024-03-13 18:37:04 -0600

    WIP RequestSQL Repo

--- back-end/src/app/domain/interfaces/repositories/IUserRequestRepository.ts
+++ back-end/src/app/domain/interfaces/repositories/IUserRequestRepository.ts
@@ -3,7 +3,7 @@ import { RequestTypeEnum } from "@app/domain/RequestTypeEnum";
 import { UserRequest } from "@app/domain/UserRequest";
 
 export interface IUserRequestRepository {
-  getAll: (requestType?: RequestTypeEnum, requestStatus?: RequestStatusEnum) => Promise<UserRequest>;
+  getAll: (requestType?: RequestTypeEnum, requestStatus?: RequestStatusEnum) => Promise<UserRequest[]>;
   get: (email: string) => Promise<UserRequest | null>;
   create: (request: UserRequest) => Promise<boolean>;
   update: (request: UserRequest) => Promise<boolean>;
