db7a069952ff7b4c0eb428f6af016faef39bf0a0
Thai <thaila71669@gmail.com>
2024-03-13 19:39:49 -0600

    fix: Survey related repo fixes

--- back-end/src/app/domain/interfaces/repositories/ISurveyQuestionRepository.ts
+++ back-end/src/app/domain/interfaces/repositories/ISurveyQuestionRepository.ts
@@ -2,8 +2,8 @@ import { SurveyQuestion } from "@app/domain/SurveyQuestion";
 
 export interface ISurveyQuestionRepository {
   getAll: () => Promise<SurveyQuestion[]>;
+  getBySurvey: (surveyId: number) => Promise<SurveyQuestion[]>;
   getByQuestionId: (questionID: number) => Promise<SurveyQuestion | null>;
-  getBySurveyId: (surveyId: number) => Promise<SurveyQuestion[] | null>;
   create: (question: SurveyQuestion) => Promise<boolean>;
   update: (question: SurveyQuestion) => Promise<boolean>;
   delete: (questionId: number) => Promise<boolean>;
