28909d61b6b91c34458f085fa9df0fe1678d577a
Aryan Mehta <aryanmht9@gmail.com>
2024-03-14 18:23:13 -0600

    saving commit

---
+++ back-end/src/app/application/UserRequestService.ts
@@ -0,0 +1,38 @@
+import { userReqRepoToken } from "@app/adapter/DependencyInjections";
+import { RequestStatusEnum } from "@app/domain/RequestStatusEnum";
+import { RequestTypeEnum } from "@app/domain/RequestTypeEnum";
+import { UserRequest } from "@app/domain/UserRequest";
+import { IUserRequestRepository } from "@app/domain/interfaces/repositories/IUserRequestRepository";
+import { inject, injectable } from "tsyringe";
+
+@injectable()
+export class UserRequestService {
+  constructor(@inject(userReqRepoToken) private readonly _userReqRepository: IUserRequestRepository) {
+
+  }
+
+  public async getAll(requestType?: RequestTypeEnum, requestStatus?: RequestStatusEnum): Promise<UserRequest[]> {
+    return this._userReqRepository.getAll(requestType);
+  }
+
+  public async get(email: string): Promise<UserRequest | null> {
+    try {
+      return this._userReqRepository.get(email);
+    } catch (error) {
+      console.error("Failed to retrieve user by email: ", error);
+      throw error;
+    }
+  };
+
+  public async create(request: UserRequest): Promise<boolean> {
+    return this._userReqRepository.create(request);
+  };
+
+  public async update(request: UserRequest): Promise<boolean> {
+    return this._userReqRepository.update(request);
+  };
+
+  public async delete(requestId: number): Promise<boolean> {
+    return this._userReqRepository.delete(requestId);
+  };
+}
