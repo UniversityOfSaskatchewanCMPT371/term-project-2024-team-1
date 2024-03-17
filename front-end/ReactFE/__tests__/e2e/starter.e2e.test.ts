3162f13d0440002048c9091e3ebad82ea4239a80
Khoa Nguyen <minhkhoanguyen.work@gmail.com>
2024-03-16 18:48:06 -0600

    chore: clean up test folder

---
+++ front-end/ReactFE/__tests__/e2e/starter.e2e.test.ts
@@ -0,0 +1,14 @@
+import { expect } from "detox";
+describe("Example", () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it("should have welcome screen", async () => {
+    await expect(element(by.text("SURVEY TEST"))).toBeVisible();
+  });
+});
