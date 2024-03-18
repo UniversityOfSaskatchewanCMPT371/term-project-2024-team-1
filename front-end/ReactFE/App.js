d79c011e6acf650144e3faee6e08196e4efbdd4e
NaronB66 <51119270+Nk-Tn-3@users.noreply.github.com>
2024-03-18 14:43:21 -0600

    ios status bar fix

--- front-end/ReactFE/App.js
+++ front-end/ReactFE/App.js
@@ -2,15 +2,17 @@
 // import { StyleSheet, Text, View } from 'react-native';
 import React from "react";
 import Index from "./app/index";
-import { View,Text } from "react-native";
+import { SafeAreaView } from "react-native";
 import { AuthProvider } from "./app/context/AuthContext";
 import { StatusBar } from "expo-status-bar";
 import * as SplashScreen from 'expo-splash-screen';
 const App = () => {
   return <AuthProvider>
     
+    <SafeAreaView style={{flex:1}}>
     <Index id={'welcome'}/>
-    <StatusBar style="light" hidden={false} translucent={false} />
+    </SafeAreaView>
+    <StatusBar style="default"  hidden={false} translucent={false}showHideTransition={false} barStyle={"rgba(127, 17, 224, 1)"} />
   </AuthProvider>;
 };
 
