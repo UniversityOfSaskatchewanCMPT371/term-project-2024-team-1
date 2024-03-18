6ded68db0295ea6d6dae291c9f3a6c5689cf518d
Khoa Nguyen <117235786+KhoaNguyen178@users.noreply.github.com>
2024-03-17 18:25:20 -0600

    End-to-end testing (#145)
    * Implementing CreateSurvey tab
    
    * Adding new question and switching type
    
    Added functionality for new question
    
    TODO:
    remove,
    add derived question,
    MC question
    
    * Added Remove functionality
    
    TODO:
    
    add derived questions
    add multiple choice variant
    Group up at the end
    
    * Finished Wrapping function when Done is clicked
    
    * Finished multiple choice question implementation
    
    Multiple  choice feature added
    
    TODO:
    Preview functionality
    connecting to API
    
    * fix: changing old e2e tests to ts
    
    * feat: add e2e-test script
    
    * fix: reorganize test files into one folder
    
    * fix: rename e2e test files for readibility
    
    * fix: changing smoke test scripts and config files
    
    * Moved the navigation panel to Admin page
    
    * chore: clean up test folder
    
    * feat: package.json script update
    
    * chore: clean up repo
    
    * feat: config babel to work with ts
    
    * feat: create survey smoke test
    
    * wip: unexepected token error
    
    * wip: add transformIgnorePatterns
    
    * Merged issue 39 into branch
    
    added conditional to bring createssurvey as main page for easier testing
    
    ---------
    
    Co-authored-by: NaronB66 <51119270+Nk-Tn-3@users.noreply.github.com>

--- front-end/ReactFE/app/navigation/Navigator.js
+++ front-end/ReactFE/app/navigation/Navigator.js
@@ -2,144 +2,213 @@ import React from "react"
 import { createDrawerNavigator } from "@react-navigation/drawer"
 import Survey from "../Screen/Survey"
 import Profile from "../Screen/Profile"
-import {createStackNavigator} from "@react-navigation/stack"
+import { createStackNavigator } from "@react-navigation/stack"
 import Login from "../Screen/Login"
 import SurveyBoard from "../Screen/SurveyBoard"
 import SurveyStartBoard from "../Screen/SurveyStartBoard"
 import CustomDrawer from "./CustomDrawer"
 import { useAuth } from "../context/AuthContext"
+import CreateSurvey from "../Screen/CreateSurvey"
 import Admin from "../Screen/Admin"
+
+
+
+/*Test constants */
+const runSurveyCreationTest = true;
+
+
+
+
+
 const Drawer = createDrawerNavigator();
 
-
+const AppSurveyStack = createStackNavigator();
+const AppMainStack = createStackNavigator();
+const AdminDrawer = createDrawerNavigator();
 
 const SurveyStack = () => {
-    return (
-      <AppSurveyStack.Navigator screenOptions={{
-        headerShown: false
-      }} initialRouteName="Survey">
-      <AppSurveyStack.Screen name="SurveyScreen" component={Survey}/>
+  return (
+    <AppSurveyStack.Navigator screenOptions={{
+      headerShown: false
+    }} initialRouteName="Survey">
+      <AppSurveyStack.Screen name="SurveyScreen" component={Survey} />
       <AppSurveyStack.Screen name="SurveyBoard" component={SurveyBoard} />
       <AppSurveyStack.Screen name="SurveyStartBoard" component={SurveyStartBoard} />
+
     </AppSurveyStack.Navigator>
-       
-    );
-  }
 
-  const MainDrawer = () =>{
+  );
+}
 
-    return (
+const AdminMainDrawer = () => {
+  return (
+
     <Drawer.Navigator testID={"drawer"} tabBarTestID={"drawer"} id={"drawer"}
-    
-    screenOptions={{
-      overlayColor:"transparent",
-      drawerStyle:{
-        backgroundColor: "#5f6fc0"
-      },
-      screenOptions:{
-        backgroundColor: "black" ,
-        fontSize:"2em",
-        
-        width:250,
-      },
-      cardStyle: {
-        backgroundColor: '#7f92f0',
-     },
-    headerShown:false,
-    drawerActiveTintColor:"#26177d",
-    drawerLabelStyle:{
-      color:"#fff",
-      fontSize:20,
-      paddingLeft:10
-    }
-    
-    
-    }
-      
-    }
 
-    drawerContent={props=><CustomDrawer {...props}  />}
-
-
-    
-    initialRouteName="Home">
-    <Drawer.Screen name ="Survey" testID={"survID"} component={SurveyStack} />
-    <Drawer.Screen name ="Profile" testID={"profileID"} component={Profile} />
-    
-    </Drawer.Navigator>
-    )
-  }
-
-  const AppSurveyStack = createStackNavigator();
-  const AppMainStack = createStackNavigator();
-
-  const MainStack = () =>{
-    const {authState} = useAuth();
-    return (
-      <AppMainStack.Navigator
       screenOptions={{
-        headerShown: false,
-        drawerStyle:{
-          backgroundColor: "#7f92f0"
+        overlayColor: "transparent",
+        drawerStyle: {
+          backgroundColor: "#5f6fc0"
         },
-        screenOptions:{
-          backgroundColor: "black" ,
-          fontSize:"2em",
-          
-          width:250,
+        screenOptions: {
+          backgroundColor: "black",
+          fontSize: "2em",
+
+          width: 250,
         },
         cardStyle: {
           backgroundColor: '#7f92f0',
-       },
-      headerStyle:{
-        backgroundColor:"#5d6ebe",
-      },
-       headerTintColor:"#fff",
-       headerTitleStyle:{
-        fontWeight:"bold"
-       },
-      drawerActiveTintColor:"#26177d",
-      drawerLabelStyle:{
-        color:"#111"
-      }}}
-    
-    initialRouteName="Home">
-      {authState?.token ? (
-        authState?.role === "ADMIN" ? (
-          <AppMainStack.Screen name="Admin" component={Admin} />
-        ) : (
-          <AppMainStack.Screen name="Home" component={MainDrawer} />
-        )
-      ) : (
-        <AppMainStack.Screen name="Login" component={Login} />
-      )}
-     
-     
-      
+        },
+        headerShown: false,
+        drawerActiveTintColor: "#26177d",
+        drawerLabelStyle: {
+          color: "#fff",
+          fontSize: 20,
+          paddingLeft: 10
+        }
+
+
+      }
+
+      }
+
+      drawerContent={props => <CustomDrawer {...props} />}
+
+
+
+      initialRouteName="AdminHome">
+      <Drawer.Screen name="AdminHome" testID={"adminHomeID"} component={Admin} />
+      <Drawer.Screen name="Create Survey" testID={"createsurvey"} component={CreateSurvey} />
+
+    </Drawer.Navigator>
+  )
+
+
+}
+
+const MainDrawer = () => {
+
+  return (
+    <Drawer.Navigator testID={"drawer"} tabBarTestID={"drawer"} id={"drawer"}
+
+      screenOptions={{
+        overlayColor: "transparent",
+        drawerStyle: {
+          backgroundColor: "#5f6fc0"
+        },
+        screenOptions: {
+          backgroundColor: "black",
+          fontSize: "2em",
+
+          width: 250,
+        },
+        cardStyle: {
+          backgroundColor: '#7f92f0',
+        },
+        headerShown: false,
+        drawerActiveTintColor: "#26177d",
+        drawerLabelStyle: {
+          color: "#fff",
+          fontSize: 20,
+          paddingLeft: 10
+        }
+
+
+      }
+
+      }
+
+      drawerContent={props => <CustomDrawer {...props} />}
+
+
+
+      initialRouteName="Home">
+      <Drawer.Screen name="Survey" testID={"survID"} component={SurveyStack} />
+      <Drawer.Screen name="Profile" testID={"profileID"} component={Profile} />
+
+    </Drawer.Navigator>
+  )
+}
+
+
+const MainStack = () => {
+  const { authState } = useAuth();
+  return (
+    <AppMainStack.Navigator
+      screenOptions={{
+        headerShown: false,
+        drawerStyle: {
+          backgroundColor: "#7f92f0"
+        },
+        screenOptions: {
+          backgroundColor: "black",
+          fontSize: "2em",
+
+          width: 250,
+        },
+        cardStyle: {
+          backgroundColor: '#7f92f0',
+        },
+        headerStyle: {
+          backgroundColor: "#5d6ebe",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold"
+        },
+        drawerActiveTintColor: "#26177d",
+        drawerLabelStyle: {
+          color: "#111"
+        }
+      }}
+
+      initialRouteName="Home">
+
+
+
+
+
+      {
+
+        runSurveyCreationTest ? <AppMainStack.Screen name="AdminDrawer" component={AdminMainDrawer} /> :
+
+          authState?.token ? (
+            authState?.role === "ADMIN" ? (
+              <AppMainStack.Screen name="AdminDrawer" component={AdminMainDrawer} />
+            ) : (
+              <AppMainStack.Screen name="Home" component={MainDrawer} />
+            )
+          ) : (
+            <AppMainStack.Screen name="Login" component={Login} />
+          )
+
+      }
+
+
     </AppMainStack.Navigator>
-      
-    )
-  }
+
+  )
+}
 
 
 
 
 const Navigator = () => {
   const { authState } = useAuth();
-  return(
-        <MainStack />
+  return (
+    <MainStack />
   );
 }
 
 export default Navigator
 
 
-  /*headerStyle:{
-          backgroundColor:"#5d6ebe",
-        },
-         headerTintColor:"#fff",
-         headerTitleStyle:{
-          fontWeight:"bold"
-         },
+/*headerStyle:{
+        backgroundColor:"#5d6ebe",
+      },
+       headerTintColor:"#fff",
+       headerTitleStyle:{
+        fontWeight:"bold"
+       },
 
-         */
+       */
