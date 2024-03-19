e5cf00145bf639e78c9dc4834aa23076fd1518fa
aaryanpatell <138918830+aaryanpatell@users.noreply.github.com>
2024-03-18 18:42:28 -0600

    Update 20240316171847-add-test-user-up.sql

--- back-end/src/resources/database/migrations/sqls/20240316171847-add-test-user-up.sql
+++ back-end/src/resources/database/migrations/sqls/20240316171847-add-test-user-up.sql
@@ -1,5 +1,5 @@
 BEGIN;
 
-INSERT INTO User (userId, password, email, clinicName, isAdmin) VALUES ("test12345", "$2a$05$nWO8LNgCKQXnD6oWr3vUAenZB1THK56RqVzzZBCEYtXFl/K3d6KaC", "test@gmail.com", 'TestClinic', TRUE);
+INSERT INTO User (userId, password, email, clinicName, isAdmin) VALUES ("test12345", "$2a$05$nWO8LNgCKQXnD6oWr3vUAenZB1THK56RqVzzZBCEYtXFl/K3d6KaC", "test@gmail.com", "TestClinic", true);
 
 COMMIT;
