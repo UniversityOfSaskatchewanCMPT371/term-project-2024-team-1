86ae5282920cc11fe367ac794ba9b03b30beb6b0
Aryan Mehta <aryanmht9@gmail.com>
2024-03-23 14:07:30 -0600

    refactor: did some minor refactoring

--- back-end/src/app/adapter/Controllers/Handlers/SurveyGetUsersSubmittedHandler.ts
+++ back-end/src/app/adapter/Controllers/Handlers/SurveyGetUsersSubmittedHandler.ts
@@ -4,15 +4,11 @@ import { nullOrUndefined } from "@app/application/util";
 import { IRouteHandler } from "@app/domain/interfaces/IRouteHandler";
 import { Request, Response } from "express";
 import { container, injectable } from "tsyringe";
-import { configure } from "log4js";
-import log4jsConfig from "@resources/log4js-config.json";
 import assert from "assert";
-// import { User } from "@app/domain/User";
 import { LoggerFactory } from "@app/domain/factory/LoggerFactory";
 import { ILogger } from "@app/domain/interfaces/ILogger";
 
 
-configure(log4jsConfig);
 
 @injectable()
 export class SurveyGetUsersSubmittedHandler implements IRouteHandler<string[] | null> {
@@ -22,27 +18,25 @@ export class SurveyGetUsersSubmittedHandler implements IRouteHandler<string[] | 
   }
 
   public handle(req: Request, res: Response): void {
-    if (this.validation(req)) {
-      this._logger.INFO("validation of the req is successful");
-      this.execute(req).then((userIds) => {
-        this._logger.INFO("Execution of getting users is successful");
-        assert(!nullOrUndefined(userIds), "array of userIds is null");
-        if (userIds.length === 0) {
-          this._logger.INFO(`Empty array returned. No users have completed the survey ${req.params.surveyId}`);
-          res.status(200).send("No users have completed the survey");
-        }
-        this._logger.INFO("Retrieved all users");
-        res.status(200).send(userIds);
-      }).catch((error: any) => {
-        this._logger.ERROR(`Failed to retrive all users, error occured: ${error}`);
-        res.status(500).send("Server failed to retrieve users, please try again.");
-      });
-
-    } else {
+    if (!this.validation(req)) {
       this._logger.INFO("Failed to get users, surveyId wasn't provided");
-      res.status(400).send("SurveyId is required");   
-    }
-
+      res.status(400).send("SurveyId is required");
+      return;   
+    } 
+    this._logger.INFO("validation of the req is successful");
+    this.execute(req).then((userIds) => {
+      this._logger.INFO("Execution of getting users is successful");
+      assert(!nullOrUndefined(userIds), "array of userIds is null");
+      if (userIds.length === 0) {
+        this._logger.INFO(`Empty array returned. No users have completed the survey ${req.params.surveyId}`);
+        res.status(200).send("No users have completed the survey");
+      }
+      this._logger.INFO("Retrieved all users");
+      res.status(200).send(userIds);
+    }).catch((error: any) => {
+      this._logger.ERROR(`Failed to retrive all users, error occured: ${error}`);
+      res.status(500).send("Server failed to retrieve users, please try again.");
+    });
   }
 
   public async execute(req: Request): Promise<string[] | null> {
