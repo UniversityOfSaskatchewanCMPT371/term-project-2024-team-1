5f3c6d1c41b5470646ba30a14528d97085e42cd4
Thai <thaila71669@gmail.com>
2024-03-25 18:45:28 -0600

    fix: issue 183 - QuestionController refactor

--- back-end/src/app/adapter/Controllers/QuestionContoller.ts
+++ back-end/src/app/adapter/Controllers/QuestionContoller.ts
@@ -1,34 +1,25 @@
-import express, { Request, Response, Router } from "express";
-import { container } from "tsyringe";
-import { QuestionGetAllHandler } from "@app/adapter/Controllers/Handlers/QuestionGetAllHandler";
 import { QuestionCreateHandler } from "@app/adapter/Controllers/Handlers/CreateQuestionHandler";
-
-const router: Router = express.Router();
-
-const questionGetAllHandler: QuestionGetAllHandler = container.resolve(QuestionGetAllHandler);
-const questionCreateHandler: QuestionCreateHandler = container.resolve(QuestionCreateHandler);
+import { QuestionGetAllHandler } from "@app/adapter/Controllers/Handlers/QuestionGetAllHandler";
+import express, { Request, Response, Router } from "express";
+import { injectable } from "tsyringe";
 
 
-// router.get("/surveys", (req: Request, res: Response) => {
-//   surveyGetAllHandler.hand(req, res);
-// });
+@injectable()
+export class QuestionController {
+  private readonly _router: Router = express.Router();
 
-// router.get("/survey/:surveyName", (req: Request, res: Response) => {
-//   surveyGetHandler.hand(req, res);
-// });
+  public constructor(private readonly _questionGetAllHandler: QuestionGetAllHandler,
+    private readonly _questionCreateHandler: QuestionCreateHandler) { }
 
-router.post("/question", (req: Request, res: Response) => {
-  void questionGetAllHandler.handle(req, res);
-});
+  public getController(): Router {
+    this._router.get("/question", (req: Request, res: Response) => {
+      this._questionGetAllHandler.handle(req, res);
+    });
 
-router.post("/question:question", (req: Request, res: Response) => {
-  void questionCreateHandler.handle(req, res);
-});
+    this._router.post("/question/:question", (req: Request, res: Response) => {
+      this._questionCreateHandler.handle(req, res);
+    });
 
-// router.delete("/surveys/:surveyName", (req: Request, res: Response) => {
-//   surveyDeleteHandler.handle(req, res);
-// });
-
-
-
-module.exports = router;
+    return this._router;
+  }
+}
