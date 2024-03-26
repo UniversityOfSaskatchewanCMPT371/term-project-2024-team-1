1c51304d0c6ae7e0f7caeedb79edd8b3abd5019e
Thai <thaila71669@gmail.com>
2024-03-25 18:13:49 -0600

    feat: issue 176 - API to get all surveys

--- back-end/src/app/adapter/Controllers/SurveyController.ts
+++ back-end/src/app/adapter/Controllers/SurveyController.ts
@@ -4,6 +4,7 @@ import { injectable } from "tsyringe";
 import { SurveyAddHandler } from "./Handlers/SurveyAddHandler";
 import { SurveyGetUsersSubmittedHandler } from "./Handlers/SurveyGetUsersSubmittedHandler";
 import { authenticate, ADMIN } from "@app/application/util";
+import { SurveyGetAllHandler } from "./Handlers/SurveyGetAllHandler";
 
 
 
@@ -12,9 +13,14 @@ export class SurveyController {
   private readonly _router: Router = express.Router();
 
   public constructor(private readonly _surveyAddHandler: SurveyAddHandler,
-    private readonly _surveyGetUsersSubmittedHandler: SurveyGetUsersSubmittedHandler) { }
+    private readonly _surveyGetUsersSubmittedHandler: SurveyGetUsersSubmittedHandler,
+    private readonly _surveyGetAllHandler: SurveyGetAllHandler) { }
 
   public getController(): Router {
+    this._router.get("/survey", (req: Request, res: Response) => {
+      this._surveyGetAllHandler.handle(req, res);
+    });
+
     this._router.post("/survey", (req: Request, res: Response) => {
       void this._surveyAddHandler.handle(req, res);
     });
