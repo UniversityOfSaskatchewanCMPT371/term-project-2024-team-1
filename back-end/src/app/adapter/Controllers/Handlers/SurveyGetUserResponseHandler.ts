4e98fa04582fb5efe42bf7855e11c676a770dc86
Khoa Nguyen <minhkhoanguyen.work@gmail.com>
2024-03-26 02:07:09 -0600

    feat: add handler to get all responses of a user to a survey

--- back-end/src/app/adapter/Controllers/Handlers/SurveyGetUserResponseHandler.ts
+++ back-end/src/app/adapter/Controllers/Handlers/SurveyGetUserResponseHandler.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { SurveyService } from "@app/application/SurveyService";
+import { SurveyResponseByUserService } from "@app/application/SurveyResponseByUserService";
+import { SurveyResponse } from "@app/application/SurveyResponseByUserService";
 import { nullOrUndefined } from "@app/application/util";
 import { LoggerFactory } from "@app/domain/factory/LoggerFactory";
 import { ILogger } from "@app/domain/interfaces/ILogger";
@@ -9,16 +10,44 @@ import { Request, Response } from "express";
 import { injectable } from "tsyringe";
 
 @injectable()
-export class SurveyGetUserResponseHandler implements IRouteHandler<string[] | null> {
-    handle(req: Request, res: Response): Promise<void> {
-        throw new Error("Method not implemented.");
+export class SurveyGetUserResponseHandler implements IRouteHandler<SurveyResponse[] | null> {
+    private readonly _logger: ILogger = LoggerFactory.getLogger(SurveyGetUserResponseHandler.name);
+    constructor(private readonly _surveyResponseByUserService: SurveyResponseByUserService) {
+    }
+    public handle(req: Request, res: Response): void {
+        if (!this.validation(req)) {
+            this._logger.INFO("Failed to get user responses, surveyId or userId wasn't provided");
+            res.status(400).send("SurveyId and UserId are required");
+            return;
+          } 
+          this._logger.INFO("Validation of the request is successful");
+          this.execute(req).then((responses) => {
+            this._logger.INFO("Execution of getting user responses is successful");
+            if (!responses || responses.length === 0) {
+              this._logger.INFO(`No responses found for survey ${req.params.surveyId} and user ${req.params.userId}`);
+              res.status(200).send("No responses found for this user and survey");
+              return;
+            }
+            this._logger.INFO("Retrieved all responses for the user and survey");
+            res.status(200).json(responses);
+          }).catch((error: any) => {
+            this._logger.ERROR(`Failed to retrieve user responses, error occurred: ${error}`);
+            res.status(500).send("Server failed to retrieve responses, please try again.");
+          });
     }
 
-    execute(req: Request): Promise<string[] | null> {
-        throw new Error("Method not implemented.");
-    }
+    public async execute(req: Request): Promise<SurveyResponse[] | null> {
+        const surveyId: number = Number(req.params.surveyId);
+        const userId: string = req.params.userId;
+        return await this._surveyResponseByUserService.getResponsesByUserForSurvey(surveyId, userId);
+    } 
 
-    validation(...args: any[]): boolean {
-        throw new Error("Method not implemented.");
-    }
+    public validation(req: Request): boolean {
+        return (
+          !nullOrUndefined(req.params) && 
+          !nullOrUndefined(req.params.surveyId) &&
+          !nullOrUndefined(req.params.userId) &&
+          !isNaN(Number(req.params.surveyId))
+        ); 
+      }
 }
--- back-end/src/app/application/SurveyResponseByUserService.ts
+++ back-end/src/app/application/SurveyResponseByUserService.ts
@@ -55,3 +55,5 @@ export class SurveyResponseByUserService {
     }
   };
 }
+export { SurveyResponse };
+
