87ec6518c92e793066c85d6ee0a47310405436fa
Thai <thaila71669@gmail.com>
2024-03-25 20:38:40 -0600

    fix: issue 175 - added note to domain object and minor fixes to Answer domain object

--- back-end/src/app/domain/SurveyAnswer.ts
+++ back-end/src/app/domain/SurveyAnswer.ts
@@ -1,12 +1,15 @@
 export class SurveyAnswer {
-  private readonly _question: string;
+  private readonly _questionId: number;
   private readonly _id: number;
   private _answer: string;
+  private _note: string;
     
-  public constructor(id: number, answer: string, question: string) {
+  public constructor(id: number, answer: string, questionId: number, note?: string)
+  public constructor(id: number, answer: string, questionId: number, note: string) {
     this._id = id;
     this._answer = answer;
-    this._question = question;
+    this._questionId = questionId;
+    this._note = note ?? null;
   }
     
   public get id(): number {       
@@ -21,7 +24,15 @@ export class SurveyAnswer {
     this._answer = newAnswer;
   }
 
-  public get question(): string {
-    return this._question;
+  public get question(): number {
+    return this._questionId;
+  }
+  
+  public set note(newNote: string) {
+    this._note = newNote;
+  }
+
+  public get note(): string {
+    return this._note;
   }
 }
--- back-end/src/app/domain/interfaces/repositories/ISurveyAnswerRepository.ts
+++ back-end/src/app/domain/interfaces/repositories/ISurveyAnswerRepository.ts
@@ -1,9 +1,9 @@
 import { SurveyAnswer } from "@app/domain/SurveyAnswer";
 
 export interface ISurveyAnswerRepository {
-  getAnswers: (userId: number) => Promise<SurveyAnswer[]>;
+  getAnswers: (userId: string) => Promise<SurveyAnswer[]>;
   get: (answerID: number) => Promise<SurveyAnswer | null>;
-  create: (userId: number, answer: SurveyAnswer) => Promise<boolean>;
+  create: (userId: string, answer: SurveyAnswer) => Promise<boolean>;
   update: (answer: SurveyAnswer) => Promise<boolean>;
   delete: (answerId: number) => Promise<boolean>;
 }
