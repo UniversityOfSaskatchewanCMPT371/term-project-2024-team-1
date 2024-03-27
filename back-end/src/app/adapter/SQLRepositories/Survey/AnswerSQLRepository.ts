85995fdfbc3f261547fcbd00a3025af72d17f46c
Thai La <thaila71669@gmail.com>
2024-03-26 20:39:44 -0600

    feat: wrote test and implementation for CreateAnswerHandler for POST /api/survey/answer route issue 173

--- back-end/src/app/adapter/SQLRepositories/Survey/AnswerSQLRepository.ts
+++ back-end/src/app/adapter/SQLRepositories/Survey/AnswerSQLRepository.ts
@@ -7,7 +7,8 @@ import { ResultSetHeader } from "mysql2";
 export class AnswerSQLRepository implements ISurveyAnswerRepository {
   private readonly _logger: ILogger = LoggerFactory.getLogger(AnswerSQLRepository.name);
 
-  private readonly _updateQuery: string = `UPDATE Answer SET answer=?, note=? WHERE id=?;`;
+  private readonly _updateQuery: string = `UPDATE Answer SET answer=?, note=? WHERE id=? AND userId=?;`;
+  private readonly _createQuery: string = "INSERT INTO Answer (answer, questionId, note, userId) VALUES (?, ?, ?, ?);";
 
   public async getAnswers(userId: string): Promise<SurveyAnswer[]> {
     throw new Error("unimplemented");
@@ -17,13 +18,21 @@ export class AnswerSQLRepository implements ISurveyAnswerRepository {
     throw new Error("unimplemented");
   }
 
-  public async create(userId: string, answer: SurveyAnswer): Promise<boolean> {
-    throw new Error("unimplemented");
+  async create(userId: string, answer: SurveyAnswer): Promise<number> {
+    try {
+      const result: ResultSetHeader[] = await query(this._createQuery,
+        [answer.answer, answer.questionId.toString(), answer.note, userId]);
+      const resultSetHeaders: ResultSetHeader = result[0];
+      return resultSetHeaders.insertId;
+    } catch (error) {
+      this._logger.ERROR(`Failed to add answers.`);
+      return Promise.reject(error);
+    }
   }
 
   public async update(answers: SurveyAnswer[]): Promise<boolean> {
 
-    const answerFields: string[][] = answers.map(answer => [answer.answer, answer.note, answer.id.toString()]);
+    const answerFields: string[][] = answers.map(answer => [answer.answer, answer.note, answer.id.toString(), answer.userId]);
     const bulkUpdateQuery: string = constructBulkQuery(this._updateQuery, answerFields);
 
     const updatedAnswers: Promise<boolean> = query(bulkUpdateQuery)
