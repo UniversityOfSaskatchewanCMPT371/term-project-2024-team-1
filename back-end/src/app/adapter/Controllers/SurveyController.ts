6f30d68ccddeca64446890f3ee85d9b9e5670260
Thai La <thaila71669@gmail.com>
2024-03-27 20:57:42 -0600

    feat: issue 177 - added handler and controller route.

--- back-end/src/app/adapter/Controllers/SurveyController.ts
+++ back-end/src/app/adapter/Controllers/SurveyController.ts
@@ -3,8 +3,9 @@ import express, { Request, Response, Router } from "express";
 import { injectable } from "tsyringe";
 import { SurveyAddHandler } from "./Handlers/SurveyAddHandler";
 import { SurveyGetUsersSubmittedHandler } from "./Handlers/SurveyGetUsersSubmittedHandler";
-import { authenticate, ADMIN } from "@app/application/util";
+import { authenticate, ADMIN, USER } from "@app/application/util";
 import { SurveyGetAllHandler } from "./Handlers/SurveyGetAllHandler";
+import { SurveyGetHandler } from "./Handlers/SurveyGetHandler";
 
 
 
@@ -14,14 +15,19 @@ export class SurveyController {
 
   public constructor(private readonly _surveyAddHandler: SurveyAddHandler,
     private readonly _surveyGetUsersSubmittedHandler: SurveyGetUsersSubmittedHandler,
-    private readonly _surveyGetAllHandler: SurveyGetAllHandler) { }
+    private readonly _surveyGetAllHandler: SurveyGetAllHandler,
+    private readonly _surveyGetHandler: SurveyGetHandler) { }
 
   public getController(): Router {
-    this._router.get("/survey", (req: Request, res: Response) => {
+    this._router.get("/survey", authenticate(USER), (req: Request, res: Response) => {
       this._surveyGetAllHandler.handle(req, res);
     });
 
-    this._router.post("/survey", (req: Request, res: Response) => {
+    this._router.get("/survey/:surveyId", authenticate(USER), (req: Request, res: Response) => {
+      void this._surveyGetHandler.handle(req, res);
+    });
+
+    this._router.post("/survey", authenticate(ADMIN), (req: Request, res: Response) => {
       void this._surveyAddHandler.handle(req, res);
     });
 
--- back-end/src/app/application/SurveyService.ts
+++ back-end/src/app/application/SurveyService.ts
@@ -1,11 +1,23 @@
-import { surveyRepoToken } from "@app/adapter/DependencyInjections";
+import { surveyAnswerRepoToken, surveyQuestionRepoToken, surveyRepoToken } from "@app/adapter/DependencyInjections";
 import { Survey } from "@app/domain/Survey";
+import { SurveyAnswer } from "@app/domain/SurveyAnswer";
+import { SurveyQuestion } from "@app/domain/SurveyQuestion";
+import { LoggerFactory } from "@app/domain/factory/LoggerFactory";
+import { ILogger } from "@app/domain/interfaces/ILogger";
+import { ISurveyAnswerRepository } from "@app/domain/interfaces/repositories/ISurveyAnswerRepository";
+import { ISurveyQuestionRepository } from "@app/domain/interfaces/repositories/ISurveyQuestionRepository";
 import { ISurveyRepository } from "@app/domain/interfaces/repositories/ISurveyRepository";
 import { delay, inject, injectable } from "tsyringe";
+import { nullOrUndefined } from "./util";
+import assert from "assert";
 
 @injectable()
 export class SurveyService {
-  constructor(@inject(delay(() => surveyRepoToken)) private readonly _surveyRepository: ISurveyRepository) {
+  private readonly _logger: ILogger = LoggerFactory.getLogger(SurveyService.name);
+
+  constructor(@inject(delay(() => surveyRepoToken)) private readonly _surveyRepository: ISurveyRepository,
+    @inject(delay(() => surveyQuestionRepoToken)) private readonly _surveyQuestionRepo: ISurveyQuestionRepository,
+    @inject(delay(() => surveyAnswerRepoToken)) private readonly _surveyAnswerRepo: ISurveyAnswerRepository) {
   }
 
   public async getAll(): Promise<Survey[]> {
@@ -16,10 +28,35 @@ export class SurveyService {
     try {
       return this._surveyRepository.getSurvey(surveyId);
     } catch (error) {
-      console.error("Failed to retrieve survey by name: ", error);
+      this._logger.ERROR(`Failed to get survey. \nError: ${error as string}`);
       throw error;
     }
   };
+
+  public async getSurveyContent(userId: string, surveyId: number): Promise<SurveyQuestion[]> {
+    const qIdToQuestion: Map<number, SurveyQuestion> = new Map<number, SurveyQuestion>();
+    const surveyQuestions: SurveyQuestion[] = await this._surveyQuestionRepo.getBySurvey(surveyId);
+    if (surveyQuestions.length === 0) {
+      return surveyQuestions;
+    }
+
+    const surveyAnswers: SurveyAnswer[] = await this._surveyAnswerRepo.getSurveyAnswers(userId, surveyId);
+
+    surveyQuestions.forEach((question: SurveyQuestion) => {
+      qIdToQuestion.set(question.id, question);
+    });
+
+    surveyAnswers.forEach((answer: SurveyAnswer) => {
+      const question: SurveyQuestion | undefined = qIdToQuestion.get(answer.questionId);
+      if (!nullOrUndefined(question)) {
+        assert(!nullOrUndefined(question));
+        question.answer = answer;
+      }
+    });
+
+
+    return surveyQuestions;
+  }
 
   public async createSurvey(survey: Survey): Promise<boolean> {
     return this._surveyRepository.createSurvey(survey);
@@ -29,13 +66,17 @@ export class SurveyService {
     try {
       return this._surveyRepository.getSurveySubmittedUsers(surveyId);
     } catch (error) {
-      console.error("Failed to retrieve users who completed the survey by surveyId: ", error);
+      this._logger.ERROR(`Failed to create survey. \nError: ${error as string}`);
       throw error;
     };
   }
 
   public async deleteSurvey(surveyId: number): Promise<boolean> {
     return this._surveyRepository.deleteSurvey(surveyId);
+  };
+
+  private setChildren(): void {
+
   };
 
 }
