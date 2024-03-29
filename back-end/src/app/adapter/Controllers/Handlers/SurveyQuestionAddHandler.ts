7e6d538b2b98cd30197d4bc2a9526c872857a454
Aryan Mehta <aryanmht9@gmail.com>
2024-03-29 15:22:53 -0600

    I189: WIP SurveyQuestionAdd Handler - able to add a single question to a survey

--- back-end/src/app/adapter/Controllers/Handlers/SurveyQuestionAddHandler.ts
+++ back-end/src/app/adapter/Controllers/Handlers/SurveyQuestionAddHandler.ts
@@ -1,38 +1,61 @@
 import { Request, Response } from "express";
-import { container, injectable } from "tsyringe";
+import { delay, inject, injectable } from "tsyringe";
 import { SurveyService } from "@app/application/SurveyService";
-import { getLogger } from "log4js";
+import { LoggerFactory } from "@app/domain/factory/LoggerFactory";
+import { ILogger } from "@app/domain/interfaces/ILogger";
+import { nullOrUndefined } from "@app/application/util";
+
 
 @injectable()
 export class SurveyQuestionAddHandler {
-  private readonly _logger = getLogger(SurveyQuestionAddHandler.name);
+  private readonly _logger: ILogger = LoggerFactory.getLogger(SurveyQuestionAddHandler.name);
 
-  constructor(private readonly _surveyService: SurveyService) {
-    this._surveyService = container.resolve(SurveyService);
+  constructor(@inject(delay(() => SurveyService)) private readonly _surveyService: SurveyService) {
   }
 
-  public async handle(req: Request, res: Response): Promise<void> {
-    try {
-      if (typeof (req.body.surveyId, req.body.questionId, req.body.rankOrder) === "number") {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        if (await this.execute(req.body.surveyId, req.body.questionId, req.body.rankOrder)) {
-          res.status(200).json({ message: "Question added to survey successfully." });
-        } else {
-          res.status(400).json({ message: "Failed to add question to survey." });
-        }
-      } 
-    } catch (error) {
-      this._logger.error("Error occurred while adding question to survey:", error);
-      res.status(500).json({ message: "Internal server error", error });
+  public handle(req: Request, res: Response): void {
+    if (!this.validation(req, res)) {
+      return; 
     }
+    this.execute(req).then((success) => {
+      if (success) { 
+        this._logger.INFO(`Successfully added question ${req.body.questionId} to the survey ${req.body.surveyId}`);
+        res.status(200).send(`Successfully added question to the survey`);
+      } else {
+        this._logger.INFO(`Failed to add question ${req.body.questionId} to the survey ${req.body.surveyId}`);
+        res.status(400).send(`Failed to add question to the survey`);
+      }
+    }).catch((err) => {
+      this._logger.ERROR(`Failed to add question ${req.body.questionId} to the survey ${req.body.surveyId}, error occured: ${err}`);
+      res.status(500).send("Server failed to process request, please try again");
+    });
   }
 
-  private async execute(surveyId: number, questionId: number, rankOrder: number): Promise<boolean> {
-    try {
-      return await this._surveyService.addQuestionToSurvey(surveyId, questionId, rankOrder);
-    } catch (error) {
-      this._logger.error("Error adding question to survey:", error);
-      throw error;
+  public async execute(req: Request): Promise<boolean> {
+    const questionId: number = Number(req.params.questionId);
+    const surveyId: number = Number(req.params.surveyId);
+    const rankOrder: number = Number(req.body.rankOrder);
+    return this._surveyService.addQuestionToSurvey(surveyId, questionId, rankOrder);
+  }
+
+  public validation(...args: any[]): boolean { 
+    const req: Request = args[0];
+    const res: Response = args[1];
+    const isValid: boolean = 
+      !nullOrUndefined(req.params) &&
+      !nullOrUndefined(req.body) &&
+      !nullOrUndefined(req.params.surveyId) &&
+      !nullOrUndefined(req.params.questionId) &&
+      !nullOrUndefined(req.body.rankOrder) &&
+      !isNaN(Number(req.params.surveyId)) &&
+      !isNaN(Number(req.params.questionId)) &&
+      !isNaN(Number(req.body.rankOrder));
+    if (isValid) {
+      this._logger.INFO("validation of the request is successful");
+      return isValid;
     }
+    this._logger.ERROR("Invalid format provided to the API");
+    res.send(422).send("Required fields not provided");
+    return isValid; 
   }
 }
--- back-end/src/app/adapter/Controllers/QuestionContoller.ts
+++ back-end/src/app/adapter/Controllers/QuestionContoller.ts
@@ -2,6 +2,7 @@ import { QuestionCreateHandler } from "@app/adapter/Controllers/Handlers/CreateQ
 import { QuestionGetAllHandler } from "@app/adapter/Controllers/Handlers/QuestionGetAllHandler";
 import express, { Request, Response, Router } from "express";
 import { injectable } from "tsyringe";
+import { SurveyQuestionAddHandler } from "./Handlers/SurveyQuestionAddHandler";
 
 
 @injectable()
@@ -9,7 +10,8 @@ export class QuestionController {
   private readonly _router: Router = express.Router();
 
   public constructor(private readonly _questionGetAllHandler: QuestionGetAllHandler,
-    private readonly _questionCreateHandler: QuestionCreateHandler) { }
+    private readonly _questionCreateHandler: QuestionCreateHandler, 
+    private readonly _surveyQuestionAddHandler: SurveyQuestionAddHandler) { }
 
   public getController(): Router {
     this._router.get("/question", (req: Request, res: Response) => {
@@ -18,6 +20,10 @@ export class QuestionController {
 
     this._router.post("/question", (req: Request, res: Response) => {
       this._questionCreateHandler.handle(req, res);
+    });
+
+    this._router.post("/question/:questionId/:surveyId", (req: Request, res: Response) => {
+      this._surveyQuestionAddHandler.handle(req, res);
     });
 
     return this._router;
