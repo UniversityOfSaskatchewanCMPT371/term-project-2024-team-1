75cdc0c51f1b78ba02bc220eaefac2e624c65e3e
evanderaimufua <Evanderaimufua00@gmail.com>
2024-04-02 15:27:43 -0600

    Chore: linting errors for PR
    linting errors for old database tests

--- back-end/src/tests/database_tests/database.test.ts
+++ back-end/src/tests/database_tests/database.test.ts
@@ -1,5 +1,5 @@
-﻿import { sqlPool } from "@app/adapter/SQLRepositories/SQLConfiguration"; // Removed unused 'query'
-import { RowDataPacket,ResultSetHeader,FieldPacket } from "mysql2";
+﻿import { sqlPool } from "@app/adapter/SQLRepositories/SQLConfiguration";
+import { RowDataPacket, ResultSetHeader, FieldPacket } from "mysql2";
 import { getLogger } from "log4js";
 
 const logger = getLogger();
@@ -19,13 +19,12 @@ describe("MySQL Database Tests", () => {
       if ("length" in result) {
         const existingTableNames: string[] = (result as RowDataPacket[]).map(row => Object.values(row)[0]);
         expect(existingTableNames).toEqual(expect.arrayContaining(tables));
-        logger.info("Table presence test passed.")
+        logger.info("Table presence test passed.");
       } else {
         throw new Error("Unexpected result format");
       }
     } catch (error) {
       logger.error("Error in table presence test. Analyze database tables.", error);
-    
     }
   });
 
@@ -36,165 +35,98 @@ describe("MySQL Database Tests", () => {
     if (validName) {
       logger.info("All table names conform to database schema naming conventions");
     } else {
-      logger.warn("One or more tables do not conform to the naming conventions ")
+      logger.warn("One or more tables do not conform to the naming conventions ");
     }
   });
 
   test("Check number of columns in User table", async () => {
     const tableName = "User";
-    const expectedColumns = 6; 
+    const expectedColumns = 6;
     const [fields] = await sqlPool.query(`DESCRIBE ${tableName}`);
-    
+
     if (!Array.isArray(fields)) {
-      throw new Error("Unexpected result format for Request table");
+      throw new Error("Unexpected result format for User table");
     }
-    
+
     expect(fields.length).toBe(expectedColumns);
     logger.info(`Number of columns in ${tableName} table is as expected.`);
   });
 
   test("Check number of columns in UserAnswerMap table", async () => {
     const tableName = "UserAnswerMap";
-    const expectedColumns = 3; 
+    const expectedColumns = 3;
     const [fields] = await sqlPool.query(`DESCRIBE ${tableName}`);
-    
+
     if (!Array.isArray(fields)) {
-      throw new Error("Unexpected result format for Request table");
+      throw new Error("Unexpected result format for UserAnswerMap table");
     }
-    
-    expect(fields.length).toBe(expectedColumns);
-    logger.info(`Number of columns in ${tableName} table is as expected.`);
-  });
- 
-  test("Check number of columns in Request table", async () => {
-    const tableName = "Request";
-    const expectedColumns = 8; 
-    const [fields] = await sqlPool.query(`DESCRIBE ${tableName}`);
-    
-    if (!Array.isArray(fields)) {
-      throw new Error("Unexpected result format for Request table");
-    }
-    
+
     expect(fields.length).toBe(expectedColumns);
     logger.info(`Number of columns in ${tableName} table is as expected.`);
   });
 
- test("Check number of columns in SurveyQuestions table", async () => {
-    const tableName = "SurveyQuestions";
-    const expectedColumns = 2; 
+  test("Check number of columns in Request table", async () => {
+    const tableName = "Request";
+    const expectedColumns = 8;
     const [fields] = await sqlPool.query(`DESCRIBE ${tableName}`);
-    
+
+    if (!Array.isArray(fields)) {
+      throw new Error("Unexpected result format for Request table");
+    }
+
+    expect(fields.length).toBe(expectedColumns);
+    logger.info(`Number of columns in ${tableName} table is as expected.`);
+  });
+
+  test("Check number of columns in SurveyQuestions table", async () => {
+    const tableName = "SurveyQuestions";
+    const expectedColumns = 2;
+    const [fields] = await sqlPool.query(`DESCRIBE ${tableName}`);
+
     if (!Array.isArray(fields)) {
       throw new Error("Unexpected result format for SurveyQuestions table");
     }
-    
-    expect(fields.length).toBe(expectedColumns);
-    logger.info(`Number of columns in ${tableName} table is as expected.`);
- });
-  
-   test("Check number of columns in Requests table", async () => {
-    const tableName = "Request";
-    const expectedColumns = 8; // Adjust as needed
-    const [fields] = await sqlPool.query(`DESCRIBE ${tableName}`);
-    
-    if (!Array.isArray(fields)) {
-      throw new Error("Unexpected result format for SurveyAnswers table");
-    }
-    
+
     expect(fields.length).toBe(expectedColumns);
     logger.info(`Number of columns in ${tableName} table is as expected.`);
   });
-  
+
   test("Check number of columns in SurveyAnswers table", async () => {
     const tableName = "SurveyAnswers";
-    const expectedColumns = 4; // Adjust as needed
+    const expectedColumns = 4;
     const [fields] = await sqlPool.query(`DESCRIBE ${tableName}`);
-    
+
     if (!Array.isArray(fields)) {
       throw new Error("Unexpected result format for SurveyAnswers table");
     }
-    
+
     expect(fields.length).toBe(expectedColumns);
     logger.info(`Number of columns in ${tableName} table is as expected.`);
-  });
-
-
-
-  test("Check column names in User table", async () => {
-    const tableName: string = "User";
-    const [fields] = await sqlPool.query(`DESCRIBE ${tableName}`);
-    const columnNames: string[] = (fields as RowDataPacket[]).map((field: any) => field.Field);
-    const expectedColumnNames: string[] = ["id", "userId", "password", "email", "isAdmin", "clinicName"];
-    expect(columnNames).toEqual(expect.arrayContaining(expectedColumnNames));
-  });
-
-  test("Check column names in UserAnswerMap table", async () => {
-    const tableName = "UserAnswerMap";
-  
-    const expectedColumnNames = ["id", "user_id","answer_id"];
-    const [fields] = await sqlPool.query(`DESCRIBE ${tableName}`);
-    const columnNames = (fields as RowDataPacket[]).map(field => field.Field);
-
-    expect(columnNames).toEqual(expect.arrayContaining(expectedColumnNames));
-    logger.info(`Column names in ${tableName} table are as expected.`);
-  });
-    
-  test("Check column names in Request table", async () => {
-    const tableName = "Request";
-  
-    const expectedColumnNames = ["id", "clinicName", "email", "password","createdDate", "decisionDate", "requestType"];
-    const [fields] = await sqlPool.query(`DESCRIBE ${tableName}`);
-    const columnNames = (fields as RowDataPacket[]).map(field => field.Field);
-
-    expect(columnNames).toEqual(expect.arrayContaining(expectedColumnNames));
-    logger.info(`Column names in ${tableName} table are as expected.`);
-  });
-
-  test("Check column names in SurveyQuestions table", async () => {
-    const tableName = "SurveyQuestions";
-  
-    const expectedColumnNames = ["id", "question"];
-    const [fields] = await sqlPool.query(`DESCRIBE ${tableName}`);
-    const columnNames = (fields as RowDataPacket[]).map(field => field.Field);
-
-    expect(columnNames).toEqual(expect.arrayContaining(expectedColumnNames));
-    logger.info(`Column names in ${tableName} table are as expected.`);
-  });
-
-  test("Check column names in SurveyAnswers table", async () => {
-    const tableName = "SurveyAnswers";
-    const expectedColumnNames = ["id", "question_id", "note"]; 
-    const [fields] = await sqlPool.query(`DESCRIBE ${tableName}`);
-    const columnNames = (fields as RowDataPacket[]).map(field => field.Field);
-
-    expect(columnNames).toEqual(expect.arrayContaining(expectedColumnNames));
-    logger.info(`Column names in ${tableName} table are as expected.`);
   });
 
   const checkColumnDetails = async (tableName: string, expectedDetails: {
-  types: Record<string, string>,
-  sizes: Record<string, number>,
-  nulls: Record<string, string>,
-  keys: Record<string, string>
-}) => {
-  const [fields, _]: [RowDataPacket[], FieldPacket[]] = await sqlPool.query(`DESCRIBE ${tableName}`) as [RowDataPacket[], FieldPacket[]];
-  
-  fields.forEach(field => {
+    types: Record<string, string>,
+    sizes: Record<string, number>,
+    nulls: Record<string, string>,
+    keys: Record<string, string>
+  }) => {
+    const [fields, _]: [RowDataPacket[], FieldPacket[]] = await sqlPool.query(`DESCRIBE ${tableName}`) as [RowDataPacket[], FieldPacket[]];
 
-    expect(field.Type).toEqual(expectedDetails.types[field.Field]);
-   
-    const sizeMatch = field.Type.match(/\((\d+)\)/);
-    if (sizeMatch && expectedDetails.sizes[field.Field]) {
-      expect(parseInt(sizeMatch[1], 10)).toEqual(expectedDetails.sizes[field.Field]);
-    }
-   
-    expect(field.Null).toEqual(expectedDetails.nulls[field.Field]);
-    
-    expect(field.Key).toEqual(expectedDetails.keys[field.Field] || '');
-  });
+    fields.forEach(field => {
+      expect(field.Type).toEqual(expectedDetails.types[field.Field]);
 
-  logger.info(`Column details for table '${tableName}' verified.`);
-};
+      const sizeMatch = field.Type.match(/\((\d+)\)/);
+      if (sizeMatch && expectedDetails.sizes[field.Field]) {
+        expect(parseInt(sizeMatch[1], 10)).toEqual(expectedDetails.sizes[field.Field]);
+      }
+
+      expect(field.Null).toEqual(expectedDetails.nulls[field.Field]);
+
+      expect(field.Key).toEqual(expectedDetails.keys[field.Field] || '');
+    });
+
+    logger.info(`Column details for table '${tableName}' verified.`);
+  };
 
   test("Verify User table column details", async () => {
     await checkColumnDetails("User", {
@@ -229,122 +161,109 @@ describe("MySQL Database Tests", () => {
   });
 
   test("Verify Request table column details", async () => {
-  await checkColumnDetails("Request", {
-    types: {
-      id: "int(11)",
-      clinicName: "varchar(255)",
-      email: "varchar(255)",
-      password: "varchar(255)",
-      status: "varchar(255)",
-      createdDate: "datetime",
-      decisionDate: "datetime",
-      requestType:"varchar(255)"
-     
-    },
-    sizes: {
-      requestType: 255,
-      status: 255,
-      clinicName: 255,
-      email: 255,
-      password: 255,
-  
-    },
-    nulls: {
-      id: "NO",
-      clinicName:"NO",
-      email: "NO",
-      password: "NO",
-      status: "NO",
-      createdDate:"NO",
-      decisionDate:"YES",
-      requestType: "NO",
-      submittedBy: "NO"
-  
-    },
-    keys: {
-      id: "PRI"
-      
-    }
+    await checkColumnDetails("Request", {
+      types: {
+        id: "int(11)",
+        clinicName: "varchar(255)",
+        email: "varchar(255)",
+        password: "varchar(255)",
+        status: "varchar(255)",
+        createdDate: "datetime",
+        decisionDate: "datetime",
+        requestType: "varchar(255)"
+      },
+      sizes: {
+        requestType: 255,
+        status: 255,
+        clinicName: 255,
+        email: 255,
+        password: 255,
+      },
+      nulls: {
+        id: "NO",
+        clinicName: "NO",
+        email: "NO",
+        password: "NO",
+        status: "NO",
+        createdDate: "NO",
+        decisionDate: "YES",
+        requestType: "NO",
+        submittedBy: "NO"
+      },
+      keys: {
+        id: "PRI"
+      }
+    });
   });
-  });
-  
 
-test("Verify SurveyQuestions table column details", async () => {
-  await checkColumnDetails("SurveyQuestions", {
-    types: {
-      id: "int(11)",
-      question: "varchar(255)",
-     
-    },
-    sizes: {
-      questionText: 255
-    },
-    nulls: {
-      id: "NO",
-      question: "NO",
-    
-    },
-    keys: {
-      id: "PRI"
-     
-    }
+
+  test("Verify SurveyQuestions table column details", async () => {
+    await checkColumnDetails("SurveyQuestions", {
+      types: {
+        id: "int(11)",
+        question: "varchar(255)",
+      },
+      sizes: {
+        question: 255
+      },
+      nulls: {
+        id: "NO",
+        question: "NO",
+      },
+      keys: {
+        id: "PRI"
+      }
+    });
   });
-});
-  
-test("Verify SurveyAnswers table column details", async () => {
-  await checkColumnDetails("SurveyAnswers", {
-    types: {
-      id: "int(11)",
-      answer: "varchar(255)",
-      question_id: "int(11)",
-      note: "varchar(255)"
-     
-    },
-    sizes: {
-      answer: 255,
-      note:255
-    },
-    nulls: {
-      id: "NO",
-      answer: "NO",
-      question_id: "YES",
-      note:"YES"
-     
-    },
-    keys: {
-      id: "PRI",
-      question_id: "MUL" 
-    }
+
+  test("Verify SurveyAnswers table column details", async () => {
+    await checkColumnDetails("SurveyAnswers", {
+      types: {
+        id: "int(11)",
+        answer: "varchar(255)",
+        question_id: "int(11)",
+        note: "varchar(255)"
+      },
+      sizes: {
+        answer: 255,
+        note: 255
+      },
+      nulls: {
+        id: "NO",
+        answer: "NO",
+        question_id: "YES",
+        note: "YES"
+      },
+      keys: {
+        id: "PRI",
+        question_id: "MUL"
+      }
+    });
   });
-});
-  
+
   test("Verify UserAnswerMap table column details", async () => {
-  await checkColumnDetails("UserAnswerMap", {
-    types: {
-      id: "int(11)",
-      user_id: "int(11)",
-      answer_id: "int(11)"
-      
-    },
-    sizes: {
-    },
-    nulls: {
-      id: "NO",
-      user_id: "YES",
-      answer_id: "YES"
-    },
-    keys: {
-      id:"PRI",
-      user_id: "MUL",
-      answer_id: "MUL"
-    }
+    await checkColumnDetails("UserAnswerMap", {
+      types: {
+        id: "int(11)",
+        user_id: "int(11)",
+        answer_id: "int(11)"
+      },
+      sizes: {},
+      nulls: {
+        id: "NO",
+        user_id: "YES",
+        answer_id: "YES"
+      },
+      keys: {
+        id: "PRI",
+        user_id: "MUL",
+        answer_id: "MUL"
+      }
+    });
   });
-});
-  
 
-    
   afterAll(async () => {
-    await sqlPool.end(); 
+    await sqlPool.end();
     //ends connection pool
   });
 });
