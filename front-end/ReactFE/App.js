32bc5b03c8310624073ee3f63bf5767c51f9f97f
ptayab <115115944+ptayab@users.noreply.github.com>
2024-04-03 10:40:46 -0600

    Fix i191 (#211)
    * fix: Refactored ViewResultsAsAdmin.js. Separated the Notifications functionality from the ViewResults Admin UI View.
    
    * Native Notifications Fixed

--- front-end/ReactFE/App.js
+++ front-end/ReactFE/App.js
@@ -5,16 +5,27 @@ import Index from "./app/index";
 import { SafeAreaView } from "react-native";
 import { AuthProvider } from "./app/context/AuthContext";
 import { StatusBar } from "expo-status-bar";
+import registerNNPushToken from 'native-notify';
 import * as SplashScreen from 'expo-splash-screen';
-const App = () => {
-  return <AuthProvider>
-    
-    <SafeAreaView style={{flex:1}}>
-    <Index id={'welcome'}/>
-    </SafeAreaView>
-    <StatusBar style="default"  hidden={false} translucent={false}showHideTransition={false} barStyle={"rgba(127, 17, 224, 1)"} />
 
-  </AuthProvider>;
+
+const useApp = () => {
+  registerNNPushToken(20557, 'FEPQ2armb5vEv4HoIDpm8x');
+  
+  return (
+    <AuthProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Index id={'welcome'} />
+      </SafeAreaView>
+      <StatusBar
+        style="default"
+        hidden={false}
+        translucent={false}
+        showHideTransition={false}
+        barStyle={"rgba(127, 17, 224, 1)"}
+      />
+    </AuthProvider>
+  );
 };
 
-export default App;
+export default useApp;
